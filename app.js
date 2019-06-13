21c6404fa1ca1ec32134df758922dc72b6f1a5b6
jasonasav <jasonasav@gmail.com>
2019-06-13 16:21:28 -0700

    added random color generator function, tested on background of cards to be used later for matchmaking.

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
     let cards = 16;
 
+    const kolor = function randomColor(){
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
     $( document ).ready(function() {
         while (cards > 0) {
-            $('.tile-container').append('<div class="tile" style="background-color: white;"></div>')
+            $('.tile-container').append('<div class="tile" style="background-color: '+kolor()+';"></div>')
             cards--;
         }
     })
