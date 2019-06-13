1b3c4d20a1e4ad607476f5e96155848b163a9dc1
jasonasav <jasonasav@gmail.com>
2019-06-13 16:05:55 -0700

    wrote basic js function to append tiles to tile container, no styling yet, number of tiles appended is hard coded but can be updated later. Currently set to 16 tiles.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() { 
 
-   console.log('hello world')
+    let cards = 16;
+
+    $( document ).ready(function() {
+        while (cards > 0) {
+            $('.tile-container').append('<div class="tile" style="background-color: white;"></div>')
+            cards--;
+        }
+    })
+
+
     
     });
