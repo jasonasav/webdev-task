b68555ea4c643feb4b813948c75cdb95b6ca53e7
jasonasav <jasonasav@gmail.com>
2019-06-19 14:23:11 -0700

    wrote function to shuffle array of random colors for front of tiles or matching side. Creates 2 ducplicate colored tiles, for a total of 16.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     let cards = 16;
 
+    // random color generator, for backs of tiles and fronts
+
     const kolor = function randomColor(){
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -10,7 +12,7 @@ $(document).ready(function() {
         }
         return color;
     }
-
+    // colors for matching tiles and backs of tiles
     let backOfTile = kolor();
     let c1 = kolor();
     let c2 = kolor();
@@ -21,15 +23,33 @@ $(document).ready(function() {
     let c7 = kolor();
     let c8 = kolor();
 
+    // array of front of tiles
+
     let tiles = [c1, c1, c2, c2, c3, c3, c4, c4, c5, c5, c6, c6, c7, c7, c8, c8];
 
-    $( document ).ready(function() {
-        while (cards > 0) {
-            $('.tile-container').append('<div class="tile" style="background-color: '+backOfTile+';"></div>')
-            cards--;
-        }
-    })
+    // shuffle/randomization of front of tiles
 
+   const shuffled = function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
 
+    shuffled(tiles);
+
+    // back of the tiles
+    //   $( document ).ready(function() {
+    //     while (cards > 0) {
+    //         $('.tile-container').append('<div class="tile" style="background-color: '+backOfTile+';"></div>')
+    //         cards--;
+    //     }
     
+    // })
+    // front of the tiles
+      $( document ).ready(function() {
+        while (cards > 0) {
+            $('.tile-container').append('<div class="tile" style="background-color: '+tiles[0]+';"></div>')
+            cards--;
+            tiles.shift();
+        }
+    
+    })
     });
